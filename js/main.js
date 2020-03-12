1fe577ecf3379a7a6742bbd1909f283a5113be01
Lera <zhidkikh.v@gmail.com>
2020-03-12 19:00:39 +0300

    Блокирует отправку формы в неактивном состоянии страницы

--- js/main.js
+++ js/main.js
@@ -194,6 +194,7 @@ renderCard(pins[0]);
 // Добавляем функцию, которая делает все input и select неактивными в исходном состоянии
 var inputs = document.querySelectorAll('input');
 var selects = document.querySelectorAll('select');
+var forms = document.querySelectorAll('form');
 var adForm = document.querySelector('.ad-form');
 var descField = adForm.querySelector('textarea');
 var buttonSubmit = adForm.querySelector('.ad-form__submit');
@@ -203,6 +204,12 @@ var mainPin = document.querySelector('.map__pin--main');
 
 
 var makeFormInactive = function () {
+  buttonSubmit.setAttribute('disabled', '');
+
+  for (var k = 0; k < forms.length; k++) {
+    forms[k].setAttribute('disabled', '');
+  }
+
   for (var i = 0; i < inputs.length; i++) {
     inputs[i].setAttribute('disabled', '');
   }
@@ -217,6 +224,11 @@ makeFormInactive();
 
 // Делаем input и select активными
 var makeFormActive = function () {
+  buttonSubmit.removeAttribute('disabled', '');
+
+  for (var k = 0; k < forms.length; k++) {
+    forms[k].removeAttribute('disabled', '');
+  }
   for (var i = 0; i < inputs.length; i++) {
     inputs[i].removeAttribute('disabled', '');
   }
