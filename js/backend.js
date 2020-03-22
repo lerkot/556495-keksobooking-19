7e28f373b045984a4b15ed31398a464341ab151e
Lera <zhidkikh.v@gmail.com>
2020-03-22 22:02:45 +0300

    Начало работы с серверными данными

---
+++ js/backend.js
@@ -0,0 +1,42 @@
+'use strict';
+(function () {
+  var URL = {
+    get: 'https://js.dump.academy/keksobooking/data',
+    post: 'https://js.dump.academy/keksobooking'
+  };
+
+  var StatusCode = {
+    OK: 200
+  };
+
+  var TIMEOUT_IN_MS = 10000;
+
+  var loadData = function (successHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        successHandler(xhr.response);
+      } else {
+        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      errorHandler('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      errorHandler('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('GET', URL.get);
+    xhr.send();
+  };
+
+  window.backend = {
+    loadData: loadData
+  };
+
+})();
